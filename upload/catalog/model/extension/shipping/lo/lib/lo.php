7270a60dc99f2e1f2ba57486c87d04f7fe7d3edc
Adrian <adrian@plationline.eu>
2024-05-17 14:44:25 +0300

    [1.2] fix printawb url

--- upload/catalog/model/extension/shipping/lo/lib/lo.php
+++ upload/catalog/model/extension/shipping/lo/lib/lo.php
@@ -139,7 +139,7 @@ class LO
 
     public function PrintAwb($f_request, $class)
     {
-        return '<a class="'.$class.'" id="print-awb" href="http://api.livrarionline.ro/Lobackend_print/PrintAwb.aspx?f_login='.$this->f_login.'&awb='.$f_request['awb'].'" target="_blank">Click pentru print AWB</a>';
+        return '<a class="'.$class.'" id="print-awb" href="https://api.livrarionline.ro/Lobackend_print/PrintAwb.aspx?f_login='.$this->f_login.'&awb='.$f_request['awb'].'" target="_blank">Click pentru print AWB</a>';
     }
 
     public function Tracking($f_request)
